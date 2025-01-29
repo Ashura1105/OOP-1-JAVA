9f1f421568708d59392e15106acf9b41e51bdcc5
Jisun Junayed <jisunjunayed@gmail.com>
2025-01-29 19:48:21 +0600

    EvaluationRectangle.java

--- LAB-2 26-01-2025/Rectangle.java
+++ LAB-2 26-01-2025/Rectangle.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-public class Main{
-    double length;
-    double width;
-    double Area;
-    public void insertData(){
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter Lenght: ");
-      length=input.nextDouble();
-      System.out.print("Enter Width: ");
-      width=input.nextDouble();
+class Main {
+    int length;
+    int height;
+    int area;
+    void Area(){
+        area= height*length;
     }
     public void display(){
-        System.out.println("Area of this Circle "+Area);
+        System.out.println("Area= " + area);
     }
-    public void area(){
-        Area=length*width;
+    void insertData(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Length: ");
+        length=input.nextInt();
+        System.out.print("Enter Height: ");
+        height=input.nextInt();
     }
-
-  public static void main(String[] args) {
-      Main r1 =new Main();
-      r1.insertData();
-      r1.area();
-      r1.display();
-  }
+    public static void main(String[] args) {
+        Main e= new Main();
+        e.insertData();
+        e.Area();
+        e.display();
+    }
 }
