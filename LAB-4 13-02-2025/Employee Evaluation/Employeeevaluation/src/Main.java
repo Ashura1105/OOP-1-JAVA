d9856bedd97b5b10d4ac49122dafd4528019fd2e
Jisun Junayed <jisunjunayed@gmail.com>
2025-02-27 00:52:27 +0600

    Add files via upload

---
+++ LAB-4 13-02-2025/Employee Evaluation/Employeeevaluation/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        HR h = new HR();
+        Soft_Engg s = new Soft_Engg();
+
+        h.display();
+        h.manageEmployee();
+
+        s.display();
+        s.developCode();
+    }
+}
